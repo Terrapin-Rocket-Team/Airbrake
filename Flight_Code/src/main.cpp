dd6dd0943b59ddac1d74b8927f3125885a1c3ecb
Michael Mallamaci <mallamacimj@gmail.com>
2024-11-26 10:25:28 -0500

    this isn't working lol

--- Flight_Code/src/main.cpp
+++ Flight_Code/src/main.cpp
@@ -17,13 +17,19 @@ const int dir_pin = 5;
 E5 enc(enc_chan_a, enc_chan_b, "E5");
 VN_100 vn(&SPI, 10);
 
-mmfs::Sensor* airbrake_sensors[2] = {&enc, &vn};
+AirbrakeState AIRBRAKE(airbrake_sensors, 2, &kf);
+
+mmfs::DPS310 baro1;
+mmfs::MS5611 baro2;
+
+mmfs::Sensor* airbrake_sensors[4] = {&baro1, &baro2, &enc, &vn};
 AirbrakeKF kf;
 mmfs::Logger logger;
-
 mmfs::ErrorHandler errorHandler;
 mmfs::PSRAM *psram;
-AirbrakeState AIRBRAKE(airbrake_sensors, 2, &kf);
+AirbrakeState AIRBRAKE(airbrake_sensors, 4, &kf);
+const int SENSOR_BIAS_CORRECTION_DATA_LENGTH = 2;
+const int SENSOR_BIAS_CORRECTION_DATA_IGNORE = 1;
 
 const int UPDATE_RATE = 10;
 const int UPDATE_INTERVAL = 1000.0 / UPDATE_RATE;
