bf5b4ff76403ee0aea511455e3e9bff2725a08e3
Michael Mallamaci <mallamacimj@gmail.com>
2025-02-17 20:09:43 -0500

    updating main with contorller code

--- Flight_Code/src/airbrake_state.h
+++ Flight_Code/src/airbrake_state.h
@@ -32,21 +32,21 @@ public:
     uint8_t currentDirection = LOW;
 
     // Flight configuation parameters
-    // double empty_mass = 40;      // in kg
-    // double target_apogee = 3100; // in m
-    // double ground_altitude = 0; // in m
+    double empty_mass = 40;      // in kg
+    double target_apogee = 3100; // in m
+    double ground_altitude = 0; // in m
 
     // Simulated parameters
-    // int max_guesses = 10;        // # of guesses before converging on desired actuation
-    // int threshold = 10;          // threshold for difference between predicted and desired apogee, (m)
-    // double angle_resolution = 5; // used in rounding desired angle to nearest increment
+    int max_guesses = 10;        // # of guesses before converging on desired actuation
+    int threshold = 10;          // threshold for difference between predicted and desired apogee, (m)
+    double angle_resolution = 5; // used in rounding desired angle to nearest increment
 
     // Airbrake Variables
     double actuationAngle = 0;            // desired actuation angle, (degrees)
-    // double estimated_apogee = 0; // in m
-    // double density = 1.225; // in kg/m^3 (this is just std atm denisty at sea level for initialization)
-    // int cda_number_of_measurements = 0;
-    // double cda_rocket;
+    double estimated_apogee = 0; // in m
+    double density = 1.225; // in kg/m^3 (this is just std atm denisty at sea level for initialization)
+    int cda_number_of_measurements = 0;
+    double cda_rocket;
 
     AirbrakeStages stage = PRELAUNCH;
 
@@ -67,10 +67,10 @@ public:
     int dir_change_time = 0;
 
     // Airbrake functions from last year
-    // int calculateActuationAngle(double altitude, double velocity, double tilt, double loop_time);
-    // double predict_apogee(double time_step,double CdA_rocket, double flap_angle,double tilt, double cur_velocity, double cur_height, double ground_alt, double empty_mass);
-    // double get_density(double h);
-    // void update_cda_estimate();
+    int calculateActuationAngle(double altitude, double velocity, double tilt, double loop_time);
+    double predict_apogee(double time_step,double CdA_rocket, double flap_angle,double tilt, double cur_velocity, double cur_height, double ground_alt, double empty_mass);
+    double get_density(double h);
+    void update_cda_estimate();
 
     double timeOfLastStage; // in seconds
 
